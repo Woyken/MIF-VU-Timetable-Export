4a29938bcb1582329462a1fca3e575fe0a099600
Karolis Užkuraitis <karolis.uzkuraitis+github@gmail.com>
2018-01-23 20:56:47 +0200

    move credential storage path to local

--- gcal-import.py
+++ gcal-import.py
@@ -36,7 +36,7 @@ def get_credentials():
     Returns:
         Credentials, the obtained credential.
     """
-    home_dir = os.path.expanduser('~')
+    home_dir = os.path.abspath("")
     credential_dir = os.path.join(home_dir, '.credentials')
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
