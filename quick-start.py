0e222949221d61639d4dd1976189ab2c8ab9c63c
Karolis Užkuraitis <karolis.uzkuraitis+github@gmail.com>
2018-01-23 08:23:51 +0200

    actual file commit

---
+++ quick-start.py
@@ -0,0 +1,92 @@
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+import datetime
+
+import json
+
+jsondata = json.load(open('lectures2018-spring.json', encoding="utf-8"))
+
+try:
+    import argparse
+
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/calendar-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/calendar'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Google Calendar API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'calendar-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else:  # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    print('Storing credentials to ' + credential_path)
+    return credentials
+
+
+def main():
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+
+    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+
+    for dataitem in jsondata:
+        desc = dataitem['eventType'] + '\n' + dataitem['lecturer'] + '\n' + dataitem['rooms']
+        title = dataitem['subjectName'] + dataitem['subgroups'] + ' (' + dataitem['subjectType'] + ')'
+        loc = dataitem['rooms']
+
+        bodyo = {
+            'end': {
+                'dateTime': dataitem['end'],
+                'timeZone': 'Europe/Vilnius'
+            },
+            'start': {
+                'dateTime': dataitem['start'],
+                'timeZone': 'Europe/Vilnius'
+            },
+            'description': desc,
+            'summary': title,
+            'location': loc
+        }
+        service.events().insert(
+            calendarId="<YOUR_CALENDAR_ID>",
+            body=bodyo).execute()
+
+
+if __name__ == '__main__':
+    main()
